780cbad529034cd29c8a40597c85678ddc49c883
ivanmakarov1 <ivanmakarov1@yandex.ru>
2022-03-28 17:52:45 +0300

    54рк

--- unit_12.js
+++ unit_12.js
@@ -193,7 +193,7 @@ let a11 = [
 function f11() {
     let out = '';
     for(let i = 0; i < a11.length; i++){
-        for(let k = a11[i].length - 1; i < k; k--){
+        for(let k = a11[i].length - 1; k >= 0; k--){
             out += a11[i][k] + ' '; 
     }
 }
